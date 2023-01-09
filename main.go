07f488bcbac1618153bb3c19ea4ed02cb12e38f2
yixuan <yixuan.coder@gmail.com>
2023-01-09 17:09:21 +0800

    brutalforce: multi-char variable and multi-digit number and minus

--- main.go
+++ main.go
@@ -36,80 +36,145 @@ After that, I try to and may be possibly come up with a relatively generic way.
 package main
 
 import (
-    // "fmt"
+    "fmt"
+    "strconv"
 )
 
 func main() {
-    input := `
+    input1 := `
 a = 1
 b = 2
 c = a + b
 `
-    brutalforce(input)
+    if env := brutalforce(input1); env["c"] != 3 {
+        fmt.Println("simplest impl failed:", env["c"], env)
+    }
 
+    input2 := `
+one = 1
+two = 2
+sum = one + two
+`
+    if env := brutalforce(input2); env["sum"] != 3 {
+        fmt.Println("multi-char variable failed:", env["sum"], env)
+    }
+
+    input3 := `
+twelve = 12
+twentyone = 21
+sum = twelve + twentyone
+`
+    if env := brutalforce(input3); env["sum"] != 33 {
+        fmt.Println("multi-digit number failed:", env["sum"], env)
+    }
+
+    input4 := `
+twelve = 12
+twentyone = 21
+sum = twentyone - twelve
+`
+    if env := brutalforce(input4); env["sum"] != 9 {
+        fmt.Println("minus-operator failed:", env["sum"], env)
+    }
+
+    input5 := `
+twelve = 12
+twentyone = 21
+sum = -twentyone - twelve
+`
+    if env := brutalforce(input5); env["sum"] != -33 {
+        fmt.Println("pre-minus-operator failed:", env["sum"], env)
+    }
 }
 
 
-func brutalforce(input string) {
+func brutalforce(input string) map[string]int {
     /*
     Here I also wanna feel the difference in difficulty between 
     doing it all at once and splitting them into more pieces of smaller function
+
+    then, implement multi-char variable and multi-digit number
 
     */
 
     env := map[string]int{}
     var curvar string
-    var curval int
+    var curval string
     var location string
     var exprval int
     var operator string = "+"
+    var tcurvar string
 
     for _, c := range input {
-        // There are still a lot of mess 
-        // So I decide to keep direction right, and simplify problems
-        // continuely simplify the rules:
-        //      single-char variable and single-digit number of value
+        // if we use this approach of if-condition, then our way of thought is 
+        // splitting variable and the others.
+        // but, it's not that normal, maybe we split it by left and right 
+        // is more suitable for us using if-condition.
+        // later, I will try it.
 
-        if c == '=' {
-            location = "right"
-            continue
-        }
-
-        if '0' <= c && c <= '9' {
-            curval = int(c - '0')
-        }
-
-        if c == ' ' {
-            continue
-        }
-
-        if c == '\n' {
-            if exprval > 0 {
-                env[curvar] = exprval
-            } else {
-                env[curvar] = curval
-            }
-
-            location = "left"
-        }
+        // And this patching-way can just resolve simple structure, 
+        // can't go far, and I also wanna see.
 
         if 'a' <= c && c <= 'z' {
             if location == "left" {
-                curvar = string(c)
+                curvar += string(c)
             } else if location == "right" {
-                if operator == "+" {
-                    exprval += env[string(c)]
+                tcurvar += string(c)
+            }
+        } else {
+            if location == "right" {
+                if tcurvar != "" {
+                    if operator == "+" {
+                        exprval += env[tcurvar]
+                    } else if operator == "-" {
+                        exprval -= env[tcurvar]
+                    }
+                    tcurvar = ""
                 }
             }
-        }
+            if c == '=' {
+                location = "right"
+                continue
+            }
 
-        if c == '+' {
-            operator = "+"
+            if '0' <= c && c <= '9' {
+                curval += string(c)
+            }
+
+            if c == ' ' {
+                continue
+            }
+
+            if c == '\n' {
+                if curvar != "" {
+                    if curval != "" {
+                        v, err := strconv.Atoi(curval)
+                        if err == nil {
+                            env[curvar] = v
+                        } else {
+                            fmt.Println("there is a error:", err)
+                        }
+                    } else {
+                        env[curvar] = exprval
+                    }
+                }
+
+                location = "left"
+
+                curvar = ""
+                tcurvar = ""
+                curval = ""
+                operator = "+"
+                exprval = 0
+            }
+
+            if c == '+' || c == '-' {
+                operator = string(c)
+            }
         }
 
     }
 
-    println(env["c"])
-
+    return env
 }
 
